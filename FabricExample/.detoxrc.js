2cfb335b308f165ef71fa9ccbd327f93a69b6f6c
Hiếu Nguyễn Đức <hieunguyenduc@Mac.lan>
2025-10-26 00:36:15 +0700

    save

--- FabricExample/.detoxrc.js
+++
@@ -1,3 +0,0 @@
-const utils = require('../scripts/detox-utils.cjs');
-module.exports = utils.commonDetoxConfigFactory('FabricExample');
-
--- FabricExample/App.tsx
+++
@@ -1,8 +0,0 @@
-import App from '../apps';
-import { featureFlags } from '../src';
-
-featureFlags.experiment.synchronousScreenUpdatesEnabled = false
-featureFlags.experiment.synchronousHeaderConfigUpdatesEnabled = false
-featureFlags.experiment.synchronousHeaderSubviewUpdatesEnabled = false
-
-export default App;
